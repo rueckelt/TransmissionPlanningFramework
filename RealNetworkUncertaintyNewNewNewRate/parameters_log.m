9ce3bd0e4eaa0a008a50ca53f7465cd4e251577d
illidan <lidan1991bupt@gmail.com>
2016-12-02 15:54:34 +0100

    greedy result for check

---
+++ RealNetworkUncertaintyNewNewNewRate/parameters_log.m
@@ -0,0 +1,12 @@
+
+% 2016-10-17 21:40:31
+
+% 17_10_uncertainty_log_f8_n3_t30/NetworkCharacUncertainty/0.3_0.0/
+max_time = 1;
+max_flows = 1;
+max_nets = 1;
+max_rep = 10;
+evaluate_max_only = 1;
+
+scheduler_logs= {'Optimization_log.m',  'true_adapted.m','false_adapted.m', 'Greedy_0_H2_log.m', 'GreedyOnline_H2_log.m', 'GreedyOnlineOpp_0_H2_log.m'}; %
+schedulers= {'Optimal', 'GA(True)','GA(False)',  'Greedy', 'GreedyOnline', 'GreedyOnlineOpp'};%
